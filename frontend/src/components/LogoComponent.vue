7308b60639c87d6ccff890cd35aee88df00e4b55
erofcon <temirlan.shereuzhev@gmail.com>
2025-11-07 17:46:26 +0300

    updated logo

--- frontend/src/components/LogoComponent.vue
+++ frontend/src/components/LogoComponent.vue
@@ -4,8 +4,9 @@
 
 <template>
   <div class="flex flex-col items-center">
-    <img class="w-35 h-35 rounded-full" src="@/assets/images/logo.jpg" alt="Rounded avatar"/>
-    <img class="max-w-50" src="@/assets/images/logo_text.png" alt="Rounded avatar"/>
+    <img class="w-35 h-35 rounded-full" src="@/assets/images/logo.png" alt="Rounded avatar"/>
+    <h1 class="text-3xl tg-text my-2">МО7О&Delta;</h1>
+<!--    <img class="max-w-50" src="@/assets/images/logo_text.png" alt="Rounded avatar"/>-->
   </div>
 </template>
 
